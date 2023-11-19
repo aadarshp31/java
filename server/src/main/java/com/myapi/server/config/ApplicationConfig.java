fde999347a329077e59f73245871d01959f668b4
Adarsh Pandey <adarshpandey.in@gmail.com>
2023-11-20 00:15:08 +0530

    add "JwtAuthenticationFilter" class for jwt based authentication

---
+++ server/src/main/java/com/myapi/server/config/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.myapi.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.myapi.server.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+  private final UserRepository userRepository;
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    return username -> userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+
+}
